be3ce0399af90a9e6484178893926e1b3b37b73d
Patrick Bellositz <e1027108@student.tuwien.ac.at>
2016-09-16 15:01:13 +0200

    now can open a window (ditched xterm); first one needs to install libghc-glib-dev and gtk2hs-buildtools (in linux, not eclipe)

--- VUPSUE3/src/Main.hs
+++ VUPSUE3/src/Main.hs
@@ -1,6 +1,18 @@
-module Main where
+import System.IO
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.IORef
+import Graphics.UI.Gtk hiding (Action, backspace)
 
-
-
-main::IO()
-main = undefined
+main :: IO ()
+main = do
+  void initGUI
+  window <- windowNew
+  
+  set window [  windowTitle := "Editor",
+                windowResizable := True,
+                windowDefaultWidth := 800,
+                windowDefaultHeight := 600 ]
+  
+  widgetShowAll window
+  mainGUI
