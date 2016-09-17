4c92d65c57615079b19dc6e77399d92b0daf25ae
patrick.bellositz <patrick.bellositz@192.168.2.2>
2016-09-17 16:26:09 +0200

    resized and formatted elements, disabling spellcheck does not work

--- VUPSUE3/src/Main.hs
+++ VUPSUE3/src/Main.hs
@@ -16,24 +16,27 @@ main = startGUI defaultConfig setup
 setup :: Window -> UI ()
 setup w = do
     -- active elements
-    return w # set title "Editor"
+    return w # set title "Editor" 
 
-    elSave   <- UI.button # set UI.text "Save"
-    elLoad   <- UI.button # set UI.text "Load"
-    elPath   <- UI.input # set UI.value "C:\\"
-    elText   <- UI.textarea
+    elSave <- UI.button # set UI.text "Save" # set style [("margin-right", "5px")]
+    elLoad <- UI.button # set UI.text "Load" # set style [("margin-right", "5px")]
+    elPath <- UI.input # set UI.value "C:\\" # set style [("width", "200px")]
+    elText <- UI.textarea # set style [("width", "100%"),("height", "100%"),("padding-left","10px"),("-webkit-box-sizing", "border-box"),
+     ("-moz-box-sizing", "border-box"),("box-sizing","border-box"),("tab-size","2"),("spellcheck","false !important"),("text-decoration","none !important"),
+     ("autocorrect","off !important"),("autocomplete","off !important")]
 
-    inputs   <- liftIO $ newIORef []
+    inputs <- liftIO $ newIORef []
     
     -- functionality
     let
         redoLayout :: UI ()
         redoLayout = void $ do
             layout <- mkLayout =<< liftIO (readIORef inputs)
-            getBody w # set children [layout]
+            getBody w # set children [layout] 
 
         mkLayout :: [Element] -> UI Element
         mkLayout xs = column $
-            [row [element elSave, element elLoad, element elPath], row [element elText] ]
+            [row [element elSave, element elLoad, element elPath] # set style [("margin-bottom", "10px")]
+            , row [element elText] # set style [("width","1000px"),("height","600px")]]
     
     redoLayout
