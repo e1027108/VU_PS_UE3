2df17e6206e8c32161aa118f1976caeb2287033a
raidsnail <raidsnail@raidsnail_PC>
2016-09-20 14:07:30 +0200

    data structure should now work

--- VUPSUE3/src/Datatypes.hs
+++ VUPSUE3/src/Datatypes.hs
@@ -1,16 +1,10 @@
-data Block = Command deriving Show
 
-data Command = Assignment Guard Colon Command | Returnstatement deriving Show
+data Block a = Empty | Command a [(Block a)] deriving Show
 
-data Guard = GuardPart1 GuardOp Expression Expression deriving Show
+data Command a = Guardcommand (Guard a) [(Command a)] | Assignment String (Expression a) | Returnstatement (Expression a) deriving Show
 
-data Expression = StringLiteral | Block | Name | Expression deriving Show
+data Guard a = G (Expression a) GuardOp (Expression a) (Maybe (Guard a)) deriving Show
 
 data GuardOp = Equals | NotEquals deriving Show
 
-data Returnstatement = R1 ReturnOperator Expression Semicolon deriving Show
-
-
-type Colon = Char
-type ReturnOperator = Char
-type Semicolon = Char
+data Expression a = StringLiteral String [String] (Maybe (Expression a)) | Block a [String] (Maybe (Expression a)) | Name String [String] (Maybe (Expression a)) | Expression [String] (Maybe (Expression a)) deriving Show
