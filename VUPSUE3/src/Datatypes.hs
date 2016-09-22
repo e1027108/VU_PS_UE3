066647a63c7c9434d19fbff8e8dbf1a99a6dcd9c
patrick.bellositz <patrick.bellositz@192.168.2.2>
2016-09-22 18:24:37 +0200

    current state of partly untested affairs

--- VUPSUE3/src/Datatypes.hs
+++ VUPSUE3/src/Datatypes.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Char
 import Debug.Trace
 
 data Block a = Empty | Command a [(Block a)] deriving Show
@@ -14,6 +15,7 @@ data Expression a = StringLiteral String [String] (Maybe (Expression a)) | Block
 {-
  TODO:
     refit every function to output a Block instead of a Bool
+    every deeper function might need a counter variable
 -}
  
 checkSyntax :: String -> Bool
@@ -34,7 +36,7 @@ removeLineComments :: String -> Integer -> String
 removeLineComments (x:xs) inQuotes
     | (length xs) == 0 = [x]
     | (x == '%') && (inQuotes == 0) = "" -- trace (show ("comment: " ++ [x] ++ xs)) ""
-    | (x == '%') && (inQuotes == 1) = "" -- trace (show ("rest: " ++ [x] ++ xs)) ([x] ++ (removeLineComments xs inQuotes))
+    | (x == '%') && (inQuotes == 1) = [x] ++ (removeLineComments xs inQuotes) -- trace (show ("rest: " ++ [x] ++ xs)) ([x] ++ (removeLineComments xs inQuotes))
     | (x == '\"') && (inQuotes == 0) = [x] ++ (removeLineComments xs 1)
     | (x == '\"') && (inQuotes == 1) = [x] ++ (removeLineComments xs 0)
     | otherwise = [x] ++ (removeLineComments xs inQuotes)
@@ -54,21 +56,35 @@ checkBlockEnd end
     | ((last end) == '}') = True
     | otherwise = False
 
--- TODO implement
+-- TODO will check first command's start and put it into the right check, this recursively puts the rest back here
 checkCommandSequence :: String -> Bool
-checkCommandSequence text = True
+checkCommandSequence text
+    | (head text) == ' ' || (head text) == '\t' || (head text) == '\n' = checkCommandSequence (tail text)
+    | (head text) == '^' = checkReturnStatement text 0 0
+    | (head text) == '[' = checkGuardCommand text 0 0
+    | (head text) == '*' || (isAlpha (head text)) = checkAssignment text 0 0
+    | otherwise = trace (show ("other char (cs): " ++ [head text])) False
 
--- TODO implement
-checkGuardCommand :: String -> Bool
-checkGuardCommand text = True
+-- TODO isolate the guard command then send back everything after
+checkGuardCommand :: String -> Integer -> Integer -> Bool
+checkGuardCommand text openBrackets openQuotes
+    | (head text) == '[' && openBrackets == 0 && openQuotes == 0 = checkGuardCommand (tail text) 1 0 -- should happen only once at start
+    | ( (head text) == '[' || (head text) == '(' || (head text) == '{' ) && openBrackets >= 1 && openQuotes == 0 = checkGuardCommand (tail text) (openBrackets + 1) openQuotes
+    | (head text) == ']' && openBrackets == 1 && openQuotes == 0 = checkCommandSequence (tail text) -- should happen only one at end
+    | ( (head text) == ']' || (head text) == ')' || (head text) == '}' ) && openBrackets >= 1 && openQuotes == 0 = checkGuardCommand (tail text) (openBrackets - 1) openQuotes
+    | (head text) == '\"' && openQuotes == 0 = checkGuardCommand (tail text) openBrackets 1
+    | (head text) == '\"' && openQuotes == 1 = checkGuardCommand (tail text) openBrackets 0
+    | otherwise = trace (show ("other char (gc): " ++ [head text])) False
 
--- TODO implement
-checkAssignment :: String -> Bool
-checkAssignment text = True
+-- TODO as above
+checkAssignment :: String -> Integer -> Integer -> Bool
+checkAssignment text openBrackets openQuotes
+    | otherwise = trace (show ("other char (as): " ++ [head text])) False
 
--- TODO implement
-checkReturnStatement :: String -> Bool
-checkReturnStatement text = True
+-- TODO as above
+checkReturnStatement :: String -> Integer -> Integer -> Bool
+checkReturnStatement text openBrackets openQuotes = True
+    | otherwise = trace (show ("other char (rs): " ++ [head text])) False
 
 -- dummy method
 checkExpression :: String -> Bool
