5b35bf9f88f683a07712c712aef88cf1f5360f5d
patrick.bellositz <patrick.bellositz@192.168.2.2>
2016-09-22 16:13:11 +0200

    now checks whether a block is a block correctly (and mostly removed non-exhaustiveness)

--- VUPSUE3/src/Datatypes.hs
+++ VUPSUE3/src/Datatypes.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Debug.Trace
 
 data Block a = Empty | Command a [(Block a)] deriving Show
 
@@ -14,28 +15,43 @@ checkSyntax :: String -> Bool
 checkSyntax text
     | length text < 2 = False
     | length text >= 2 = (checkBlock (removeComments text))
-    
+
 removeComments :: String -> String
-removeComments text = (removeCommentsLineByLine (lines text)) -- should now work whole text now
+removeComments text = (removeCommentsLineByLine (lines text))
 
 removeCommentsLineByLine :: [String] -> String
-removeCommentsLineByLine list = (removeLineComments (head list) 0) ++ (removeCommentsLineByLine (tail list))
+removeCommentsLineByLine list
+    | (length list) == 0 = ""
+    | (length (head list)) == 0 = (removeCommentsLineByLine (tail list))
+    | otherwise = (removeLineComments (head list) 0) ++ (removeCommentsLineByLine (tail list)) --TODO whole thing now has no '\n'
 
---this is non-exhaustive for ghci use, since this would need an additional escape character, but works with files
 removeLineComments :: String -> Integer -> String
 removeLineComments (x:xs) inQuotes
-    | (x == '%') && (inQuotes == 0) = ""
+    | (length xs) == 0 = [x]
+    | (x == '%') && (inQuotes == 0) = trace (show ("comment: " ++ [x] ++ xs)) ""
+    | (x == '%') && (inQuotes == 1) = trace (show ("rest: " ++ [x] ++ xs)) ([x] ++ (removeLineComments xs inQuotes))
     | (x == '\"') && (inQuotes == 0) = [x] ++ (removeLineComments xs 1)
     | (x == '\"') && (inQuotes == 1) = [x] ++ (removeLineComments xs 0)
-    | otherwise = [x] ++ (removeLineComments xs 0)
+    | otherwise = [x] ++ (removeLineComments xs inQuotes)
     
 checkBlock :: String -> Bool
-checkBlock text
-    | otherwise = True
+checkBlock text = checkBlockStart text
+    
+checkBlockStart :: String -> Bool
+checkBlockStart (x:xs)
+    | (x == ' ') = checkBlockStart xs
+    | (x == '{') = checkBlockEnd xs
+    | otherwise = False
+    
+checkBlockEnd :: String -> Bool
+checkBlockEnd end
+    | ((last end) == ' ') = checkBlockEnd (init end)
+    | ((last end) == '}') = True
+    | otherwise = False
     
 parseFile :: IO ()
 parseFile = do
     content <- readFile "..\\non_trivial_example.txt" --later this will load from UI
-    --print (removeComments content) -- comment for use/decomment for testing, TODO currently creates non-exhaustive error
+    -- print (removeComments content) -- TODO no non-exhaustiveness left, but if this is executed, the line below never prints True/False
     print (checkSyntax content)
     
